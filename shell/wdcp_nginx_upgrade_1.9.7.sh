c39e2d68d4b06c41e98b9529174996846af486af
lincanbin <lincanbin@hotmail.com>
2015-11-21 16:13:19 +0800

    Enable Module ngx_http_v2_module.

---
+++ shell/wdcp_nginx_upgrade_1.9.7.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# nginx update scripts
+# https://www.94cb.com/t/3393
+if [ ! $1 ];then
+    ver="1.9.7"
+else
+    ver=$1
+fi
+cd /tmp
+wget http://down.wdlinux.cn/in/pcre_ins.sh
+sh pcre_ins.sh
+wget -c http://nginx.org/download/nginx-$ver.tar.gz
+[ $? != 0 ] && echo "down err" && exit
+tar zxvf nginx-$ver.tar.gz
+cd nginx-$ver
+./configure --user=www --group=www --prefix=/www/wdlinux/nginx-$ver --with-http_stub_status_module --with-http_ssl_module --with-ipv6 --with-http_v2_module
+[ $? != 0 ] && echo "configure err" && exit
+make
+[ $? != 0 ] && echo "make err" && exit
+make install
+[ $? != 0 ] && echo "make install err" && exit
+mv /www/wdlinux/nginx-$ver/conf/nginx.conf /www/wdlinux/nginx-$ver/conf/nginx.conf.default
+cp -pR /www/wdlinux/nginx/conf/* /www/wdlinux/nginx-$ver/conf/
+service nginxd stop
+rm -f /www/wdlinux/nginx
+ln -sf /www/wdlinux/nginx-$ver /www/wdlinux/nginx
+sed -i '/limit_zone/d' /www/wdlinux/nginx/conf/nginx.conf
+service nginxd start
+echo
+echo
+echo "nginx Update Is OK"
+echo "Current Ver Is:$ver"
+echo
